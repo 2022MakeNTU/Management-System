c703459402dbebbf36790c5e8b1d920648762604
kelly <kellyworld3@gmail.com>
2022-02-19 22:02:44 +0800

    Manager-#5 UI

--- manager/src/components/Item/index.js
+++ manager/src/components/Item/index.js
@@ -1,21 +1,22 @@
-import Box from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import { color } from "@mui/system";
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
+import Button from '@mui/material/Button'
+import { color } from '@mui/system'
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  textAlign: 'center',
   color: theme.palette.text.secondary,
-}));
+}))
 function CircularProgressWithLabel(props) {
   return (
-    <Box sx={{ position: "relative", display: "inline-flex" }}>
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
       <CircularProgress
         variant="determinate"
         value={props.value}
@@ -27,10 +28,10 @@ function CircularProgressWithLabel(props) {
           left: 0,
           bottom: 0,
           right: 0,
-          position: "absolute",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
         <Typography variant="caption" component="div" color="text.secondary">
@@ -38,26 +39,27 @@ function CircularProgressWithLabel(props) {
         </Typography>
       </Box>
     </Box>
-  );
+  )
 }
 
 export default function ItemBox() {
-  const [progress, setProgress] = useState(10);
+  const [progress, setProgress] = useState(10)
 
   useEffect(() => {
     const timer = setInterval(() => {
       setProgress((prevProgress) =>
-        prevProgress <= 0 ? 100 : prevProgress - 10
-      );
-    }, 800);
+        prevProgress <= 0 ? 100 : prevProgress - 10,
+      )
+    }, 800)
     return () => {
-      clearInterval(timer);
-    };
-  }, []);
-  const _color = progress > 30 ? "green" : "red";
+      clearInterval(timer)
+    }
+  }, [])
+  const _color = progress > 30 ? 'green' : 'red'
   return (
     <Item>
       <CircularProgressWithLabel value={progress} color={_color} />
+      <Button>refill</Button>
     </Item>
-  );
+  )
 }
--- manager/src/containers/GridContainer/index.js
+++ manager/src/containers/GridContainer/index.js
@@ -1,23 +1,92 @@
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import ItemBox from "../../components/Item";
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import ItemBox from '../../components/Item'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+
 export default function GridContainer() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={2}>
-          <ItemBox />
+    <Box sx={{ width: '100%' }} backgroundColor="#e3f2fd">
+      <Stack direction="column" spacing={4} justifyContent="center">
+        <Stack justifyContent="center" direction="row" backgroundColor="white">
+          <Typography variant="h5">Tissue Inventory</Typography>
+        </Stack>
+
+        <Grid
+          container
+          rowSpacing={0}
+          columnSpacing={{ xs: 1, sm: 0, md: 0 }}
+          justifyContent="center"
+        >
+          <Stack justifyContent="center" direction="row" spacing={2}>
+            <Typography>3F</Typography>
+          </Stack>
+          <Grid container justifyContent="center">
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <ItemBox />
+
+        <Grid
+          container
+          rowSpacing={0}
+          columnSpacing={{ xs: 1, sm: 0, md: 0 }}
+          justifyContent="center"
+        >
+          <Stack justifyContent="center" direction="row" spacing={2}>
+            <Typography>2F</Typography>
+          </Stack>
+          <Grid container justifyContent="center">
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <ItemBox />
+
+        <Grid
+          container
+          rowSpacing={0}
+          columnSpacing={{ xs: 1, sm: 0, md: 0 }}
+          justifyContent="center"
+        >
+          <Stack justifyContent="center" direction="row" spacing={2}>
+            <Typography>1F</Typography>
+          </Stack>
+          <Grid container justifyContent="center">
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+            <Grid item xs={2.3}>
+              <ItemBox />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <ItemBox />
-        </Grid>
-      </Grid>
+      </Stack>
     </Box>
-  );
+  )
 }
